b631d3767798a70e1a6a24f0d148cbe751bc8975
golamrosul68 <golamrosul68990@gmail.com>
2026-07-30 17:26:31 +0600

    feat: initialize news-app with Express server and package.json setup

--- client/src/dashboard/layout/Header.jsx
+++ client/src/dashboard/layout/Header.jsx
@@ -1,21 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
   return (
     <input
       type="text"
       placeholder="Search..."
-      className="px-3 py-2 rounded-md outline-0 border border-gray-300 focus:border-green-500 h-10"
+      className="px-3 py-2 rounded-md outline-none border border-gray-300 focus:border-green-500 h-10"
     />
-
-
-    div className="ml-64 p-4">
-    
-    <div>
-      
-      </div>
-      
-      <div/>
   );
 };
 
