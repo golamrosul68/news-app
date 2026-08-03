61f3e8452471c9a1cf9a39a5c7f6e973c021b5f8
golamrosul68 <golamrosul68990@gmail.com>
2026-08-03 21:18:53 +0600

    Add dashboard sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "./dashboard/layout/MainLayout";
 import Adminindex from "./dashboard/pages/Adminindex";
+import Login from "./dashboard/pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
+
+        <Route path="/login" element={<Login />} />
 
         <Route path="/dashboard" element={<MainLayout />}>
           <Route index element={<Navigate to="/dashboard/admin" replace />} />
--- client/src/middleware/protectDashbord.jsx
+++ client/src/middleware/protectDashbord.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const protectDashbord = () => {
-  return (
+  return (  
     <div>protectDashbord</div>
   )
 }
